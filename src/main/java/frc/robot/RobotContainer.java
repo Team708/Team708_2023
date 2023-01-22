4bcc20b527afeaf7b7e9086ec8fb7042a619ac7c
LanceGuy5 <lanceforresth@gmail.com>
2023-01-21 20:06:28 -0500

    Repopulate

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,90 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.GoalConstants;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.Utilities.JoystickLeftTrigger;
+import frc.robot.Utilities.JoystickRightTrigger;
+import frc.robot.commands.DriveByController;
+import frc.robot.subsystems.Swerve.Drivetrain;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+  private final XboxController m_operatorController = new XboxController(OIConstants.kOperatorControllerPort);
+
+  private final Drivetrain m_drive = new Drivetrain();
+
+  private final DriveByController m_driveByController 
+    = new DriveByController(m_drive, m_driverController);
+
+  private final Command doNothin = new WaitCommand(20.0);
+
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+    configureAutoChooser();
+
+    m_drive.setDefaultCommand(m_driveByController);
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+
+    new POVButton(m_driverController, 0)
+        .onTrue(new InstantCommand(() -> m_drive.resetOdometry(new Rotation2d(0.0))));
+
+  }
+
+
+  private void configureAutoChooser(){
+    m_chooser.addOption("Do Nothing", doNothin);
+    
+    m_chooser.setDefaultOption("Do Nothing", doNothin);
+    SmartDashboard.putData(m_chooser);  
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_chooser.getSelected();
+  }
+
+}
