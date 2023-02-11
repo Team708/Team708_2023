7c7adfd9ea5c728526cb8b33a560a678b198ee7d
LanceGuy5 <lanceforresth@gmail.com>
2023-02-11 12:08:55 -0500

    Update RollerIntakeCommand.java

--- src/main/java/frc/robot/commands/rollerIntake/RollerIntakeCommand.java
+++ src/main/java/frc/robot/commands/rollerIntake/RollerIntakeCommand.java
@@ -5,7 +5,7 @@
 package frc.robot.commands.rollerIntake;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.RollerIntake;
+import frc.robot.subsystems.intake.RollerIntake;
 
 public class RollerIntakeCommand extends CommandBase {
   /** Creates a new Intake. */
