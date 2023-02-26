3d770840935143b688331470bfefb256d623cd3c
jpiergal <jpiergal@gmail.com>
2023-02-25 19:30:51 -0500

    last set of changes

--- src/main/java/frc/robot/commands/Autos/DriveToPieceAuto.java
+++ src/main/java/frc/robot/commands/Autos/DriveToPieceAuto.java
@@ -15,12 +15,14 @@ import frc.robot.subsystems.drive.Drivetrain;
 public class DriveToPieceAuto extends SequentialCommandGroup {
 
   public DriveToPieceAuto(Drivetrain dr, double maxSpeed) {
-    AutoFromPathPlanner path = new AutoFromPathPlanner(dr, "DriveToPiece", -maxSpeed, true);
+    AutoFromPathPlanner path = new AutoFromPathPlanner(dr, "DriveToPiece", maxSpeed, true);
     addCommands(
-      new ResetDriveCommand(dr, new Rotation2d()),
-      new InvertDriveCommand(dr),
-      path,
-      new InvertDriveCommand(dr)
+      new InstantCommand(() -> dr.resetOdometry(path.getInitialPose())),
+
+      // new ResetDriveCommand(dr, new Rotation2d()),
+      // new InvertDriveCommand(dr),
+      path
+      // new InvertDriveCommand(dr)
       );
   }
     
